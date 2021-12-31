6c06a21ccc44765fa1406affad85134725000338
Akash1012 <gupta.akash2090@gmail.com>
2021-12-31 21:07:59 +0530

    Dynamic form

--- src/Compoments/DynamicField/important.js
+++ src/Compoments/DynamicField/important.js
@@ -41,25 +41,22 @@ function PhoneBookForm(props) {
   });
 
   const handleChangeInput = (index, event) => {
-    // const values = [...inputFields];
-    console.log(event.target.name);
-    // values[index][event.target.name] = event.target.value
-    setInputFields({ ...inputFields, [event.target.name]: event.target.value });
-    // setInputFields(values)
-    // console.log("Value", inputFields);
+    setInputFields({
+      ...inputFields,
+      [event.target.name]: event.target.value,
+      mark: true,
+    });
   };
 
   const onSubmit = (e) => {
     const { save } = props;
     e.preventDefault();
     save(inputFields);
-    // setData([...data,[e.target.name] = e.target.name])
     setInputFields({
       userFirstname: "",
       userLastname: "",
       userPhone: "",
     });
-    console.log("Submit Data", inputFields);
   };
   return (
     <form onSubmit={onSubmit} style={style.form.container}>
@@ -110,15 +107,33 @@ function InformationTable(props) {
   const { saveData } = props;
   const [saveData1, setSaveData1] = useState([]);
   React.useEffect(() => {
-    console.log("Hello World");
     setSaveData1(saveData);
   }, [saveData]);
 
+  console.log("Set", saveData1);
+
   const changeInputField = (event, index) => {
-    const values = [...saveData1];
-    values[index][event.target.name] = event.target.value;
-    setSaveData1(values);
-    console.log("Value", values);
+    let items = [...saveData1];
+    // 2. Make a shallow copy of the item you want to mutate
+    let item = { ...items[index] };
+    // 3. Replace the property you're intested in
+    // debugger;
+    item[event.target.name] = event.target.value;
+    // 4. Put it back into our array. N.B. we *are* mutating the array here, but that's why we made a copy first
+    items[index] = item;
+    // 5. Set the state to our new copy
+    // console.log("Items", items);
+    setSaveData1(items);
+  };
+
+  const changeData = (index) => {
+    console.log("Index", index);
+    let items = [...saveData1];
+    let item = { ...items[index] };
+    item["mark"] = !item.mark;
+    items[index] = item;
+    setSaveData1(items);
+    console.log("Items", items);
   };
   return (
     <table style={style.table} className="informationTable">
@@ -127,20 +142,62 @@ function InformationTable(props) {
           <th style={style.tableCell}>First name</th>
           <th style={style.tableCell}>Last name</th>
           <th style={style.tableCell}>Phone</th>
+          <th style={style.tableCell}>Edit</th>
         </tr>
         {saveData1?.map((data, index) => (
           <tr key={index}>
             <td style={style.tableCell}>
-              {data.userFirstname}
-              <input
-                type="text"
-                name="userFirstname"
-                value={data.userFirstname}
-                onChange={(e) => changeInputField(e, index)}
-              />
+              {data.mark ? (
+                data.userFirstname
+              ) : (
+                <input
+                  type="text"
+                  // disabled={data.mark}
+                  name="userFirstname"
+                  value={data.userFirstname}
+                  onChange={(e) => changeInputField(e, index)}
+                />
+              )}
             </td>
-            <td style={style.tableCell}>{data.userLastname}</td>
-            <td style={style.tableCell}>{data.userPhone}</td>
+            <td style={style.tableCell}>
+              {data.mark ? (
+                data.userLastname
+              ) : (
+                <input
+                  style={style.form.inputs}
+                  className="userLastname"
+                  name="userLastname"
+                  disabled={data.mark}
+                  type="text"
+                  value={data.userLastname}
+                  onChange={(e) => changeInputField(e, index)}
+                />
+              )}
+            </td>
+            <td style={style.tableCell}>
+              {" "}
+              {data.mark ? (
+                data.userPhone
+              ) : (
+                <input
+                  style={style.form.inputs}
+                  className="userPhone"
+                  name="userPhone"
+                  value={data.userPhone}
+                  type="text"
+                  onChange={(e) => changeInputField(e, index)}
+                />
+              )}
+            </td>
+            <td style={style.tableCell}>
+              <button
+                type="button"
+                disabled={false}
+                onClick={() => changeData(index)}
+              >
+                {data.mark ? "Re-assign" : "Submit"}
+              </button>
+            </td>
           </tr>
         ))}
       </thead>
@@ -151,7 +208,7 @@ function InformationTable(props) {
 function Application(props) {
   const [saveData, setSaveData] = React.useState([]);
   const save = (data) => {
-    setSaveData([...saveData, { ...data }]);
+    setSaveData([...saveData, data]);
     console.log(saveData);
   };
   return (
